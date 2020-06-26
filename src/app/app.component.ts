a5549088d6d23e53c304d8e6ea3d7c1ceccbf25f
Collins Thomas <thomascollins582@gmail.com>
2020-06-26 09:08:46 +0100

    Added Logout Method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { AngularFireFunctions } from '@angular/fire/functions';
 import { async } from '@angular/core/testing';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +14,18 @@ import { async } from '@angular/core/testing';
 
 export class AppComponent {
 
-  constructor(private fns: AngularFireFunctions) {
+  constructor(private fns: AngularFireFunctions,
+    private afAuth: AngularFireAuth,
+    private matSnackbar: MatSnackBar) {
     // const addAdminRole = this.fns.httpsCallable('addAdminRole');
     // addAdminRole('admin@admin.com').subscribe(value => console.log(value))
+  }
+
+  async logout(): Promise<void> {
+    await this.afAuth.signOut();
+    this.matSnackbar.open('Logout Successful', 'Close', {
+      duration: 2000
+    })
   }
 
 }
