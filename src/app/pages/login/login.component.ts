d421980fe1454faec26a0bd7b4f7b5377054b6ff
Collins Thomas <thomascollins582@gmail.com>
2020-06-26 16:55:08 +0100

    Added Google Login Method

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { faGoogle, faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -15,19 +16,34 @@ export class LoginComponent implements OnInit {
 
   hide: boolean = true;
   loginForm: FormGroup;
-  constructor(private fb: FormBuilder, private authService: AuthService) {
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private matSnackbar: MatSnackBar) {
     this.loginForm = this.fb.group({
       email: ['', Validators.compose([Validators.required, Validators.email])],
       password: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
     })
-   }
+  }
 
   ngOnInit(): void {
   }
 
   login() {
-    const {email, password} = this.loginForm.value;
+    const { email, password } = this.loginForm.value;
     this.authService.loginWithEmailAndPassword(email, password)
+  }
+
+  async googleLogin() {
+    try {
+      await this.authService.googleLogin();
+      this.matSnackbar.open('Login Successful', 'Close', {
+        duration: 3000
+      })
+    } catch (error) {
+      this.matSnackbar.open(error.message, 'Close', {
+        duration: 6000
+      })
+    }
   }
 
 }
